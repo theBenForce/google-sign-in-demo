f4fca0fd67e5fb971e157566a1e772f2afc9f148
Ben Force <benforce@gmail.com>
2021-07-12 10:14:10 -0500

    feat: add auth user permission

--- lib/MyStack.ts
+++ lib/MyStack.ts
@@ -1,4 +1,5 @@
 import * as sst from "@serverless-stack/resources";
+import * as iam from "@aws-cdk/aws-iam";
 
 export default class MyStack extends sst.Stack {
     constructor(scope: sst.App, id: string, props?: sst.StackProps) {
@@ -17,5 +18,28 @@ export default class MyStack extends sst.Stack {
         });
 
         const bucket = new sst.Bucket(this, `PhotoBucket`, {});
+
+        auth.attachPermissionsForAuthUsers([
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+                resources: [
+                    `${bucket.bucketArn}/private/\${cognito-identity.amazonaws.com:sub}/*`,
+                ],
+            }),
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ["s3:ListBucket"],
+                resources: [bucket.bucketArn],
+                conditions: {
+                    StringLike: {
+                        s3prefix: [
+                            "private/${cognito-identity.amazonaws.com:sub}/",
+                            "private/${cognito-identity.amazonaws.com:sub}/*",
+                        ],
+                    },
+                },
+            }),
+        ]);
     }
 }
